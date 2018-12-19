efc7b8ef4c69871653dd919e4abfe7319fa616d3
Dixie Korley <dixiekorley@Edmunds-Macbook-Pro.local>
2018-12-19 10:58:00 -0800

    add permissons for python script

--- script_exercise.py
+++ script_exercise.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
 import os
-
-
  
-os.system("grep -oh '\w*love\w*' sonnet.txt > answer.txt && wc -w answer.txt")
+os.system("grep -oh '\w*love\w*' sonnet.txt > answer.txt && wc -w answer.txt") 
+os.system("grep -oh '\w*thee\w*' sonnet.txt > answer2.txt && wc -w answer2.txt")
+os.system("grep -oh '\w*to\w*' sonnet.txt > answer3.txt && wc -w answer3.txt")
+os.system("grep -oh '\w*eternal\w*' sonnet.txt > answer4.txt && wc -w answer4.txt")
